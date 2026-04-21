edd963f534368e3e7a2ff89ea8cdcb871a62b6dd
ionplatox <260461605+ionplatox-blip@users.noreply.github.com>
2026-04-21 18:22:44 +0300

    Entropy Engine: memorable password generator PWA

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'entropy-engine-v3';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  'https://fonts.googleapis.com/css2?family=Unbounded:wght@400;600;900&family=IBM+Plex+Mono:wght@500&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
